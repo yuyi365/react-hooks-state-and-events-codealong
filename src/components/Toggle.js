f7cef6551841a662883c1529cb5774759dc5969b
Yuyi <yuyi365@gmail.com>
2021-11-01 10:35:26 -0400

    commit tree 3eacc4b709e71ac9f33a1db2597809138fbc7b08

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
-import React from "react";
+/*
+  PROPS vs. STATE
+  🚫 Is it passed as a prop?
+  🚫 Can you compute it based on any other state or props in your component?
+  🚫 Does it remain unchanged over time?
+*/
 
-function Toggle() {
-  return <button>OFF</button>;
+import React, { useState } from "react";
+
+const Toggle = () =>  {
+
+  const [isOn, setIsOn] = useState(false); // false because the button should be "OFF" when the component first renders
+
+  const handleClick = () => {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white"; // no new state is needed because the change is dependent on another state/prop in the component
+
+  return <button style = {{background : color}} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
